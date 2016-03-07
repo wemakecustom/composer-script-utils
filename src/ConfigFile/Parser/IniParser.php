8a25db278f38823c058e5114f38bd8e9efabbcf4
Mathieu Lemoine <mathieu@wemakecustom.com>
2016-03-07 10:24:31 -0500

    PHP < 5.6 Compatibility

--- src/ConfigFile/Parser/IniParser.php
+++ src/ConfigFile/Parser/IniParser.php
@@ -9,6 +9,23 @@ class IniParser implements ParserInterface
         return function_exists('parse_ini_string');
     }
 
+    protected function flatten($values, $prefix = '')
+    {
+        $result = array();
+
+        foreach ($values as $key => $value) {
+            if (is_array($value)) {
+                foreach ($this->flatten($value, $key.'.') as $k => $v) {
+                    $result[$prefix.$k] = $v;
+                }
+            } else {
+                $result[$prefix.$key] = $value;
+            }
+        }
+
+        return $result;
+    }
+    
     protected function dumpValue($value)
     {
         if (true === $value) {
@@ -22,19 +39,45 @@ class IniParser implements ParserInterface
         return $value;
     }
 
+    protected function parseValue($value)
+    {
+        if ('true' === $value) {
+            return true;
+        } elseif ('false' === $value) {
+            return false;
+        } elseif ('null' === $value) {
+            return null;
+        } elseif (''.($val = intval($value)) === $value) {
+            return $val;
+        } elseif (''.($val = floatval($value)) === $value) {
+            return $val;
+        }
+
+        return $value;
+    }
+    
     public function dump(array $params)
     {
         $ini = array();
 
-        foreach ($params as $key => $value) {
-            $ini[] = $key.'='.$this->dumpValue($value);
+        foreach ($this->flatten($params) as $k => $v) {
+            $ini[] = $k.'='.$this->dumpValue($v);
         }
-
-        return "; This file was auto-generated during composer install\n".implode("\n", $ini);
+        
+        return "; This file was auto-generated during composer install\n"
+              .implode("\n", $ini);
     }
 
     public function parse($content)
     {
-        return parse_ini_string($content, false, constant(defined('INI_SCANNER_TYPED') ? 'INI_SCANNER_TYPED' : 'INI_SCANNER_NORMAL'));
+        $autoParsing = defined('INI_SCANNER_TYPED');
+
+        $ini = parse_ini_string($content, false, constant($autoParsing ? 'INI_SCANNER_TYPED' : 'INI_SCANNER_RAW'));
+
+        if (!$autoParsing) {
+            $ini = array_map(array($this, 'parseValue'), $ini);
+        }
+
+        return $ini;
     }
 }
